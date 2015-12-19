824b9f9c6f3294b8bae22a11e7155cbeaafc832e
Thao <theo@futureworkz.com>
2015-12-19 14:45:03 +0700

    remove row no need

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -6,7 +6,7 @@ describe Student do
     it { is_expected.to validate_presence_of :dob }
   end
   context 'validate dob greater than or equal 18' do
-    let!(:student) {Student.new(name: "John", dob: Time.now)}
+    let!(:student) { Student.new(name: "John", dob: Time.now) }
     it "must be greater than or equal 18 age" do
       student.validate
       expect(student.errors.messages).to include(dob: ['must be greater than or equal 18 age'])
