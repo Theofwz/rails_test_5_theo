b163db8ff13440875113588351608f36ce15e422
Thao <theo@futureworkz.com>
2015-12-19 12:55:32 +0700

    update db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,13 @@
 School.destroy_all
 Student.destroy_all
 OfficeWorker.destroy_all
+StudentInfomation.destroy_all
 
-School.create(name: "Futureworkz", address: "184/17, Dien Bien Phu", school_type: :university)
+futureworkz =  School.create(name: "Futureworkz", address: "184/17, Dien Bien Phu", school_type: :university)
+fwz = School.create(name: "FWZ", address: "184/17, Dien Bien Phu", school_type: :primary_school)
 
-Student.create(name: "John", dob: "1-1-1992")
+futureworkz.students.create(name: "John", dob: "1-1-1992")
+futureworkz.students.create(name: "Smith", dob: "1-3-1992")
+fwz.students.create(name: "Oliver", dob: "1-2-1992")
 
 OfficeWorker.create(name: "Alex", dob: "1-1-1993", year_of_exp: 2)
--- spec/models/student_infomation_spec.rb
+++ spec/models/student_infomation_spec.rb
@@ -1,10 +1,8 @@
 require "rails_helper"
 
 describe StudentInfomation do 
-  
   context 'Association' do
-    it { is_expected.to belong_to (:shool) }
+    it { is_expected.to belong_to (:school) }
     it { is_expected.to belong_to (:student) }
   end
-
 end
