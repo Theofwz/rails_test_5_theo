50f290ff486b76a3aa8443e57bebfb7267abbdce
Thao <theo@futureworkz.com>
2015-12-19 10:32:18 +0700

    add timestamps to students

---
+++ db/migrate/20151219033106_add_timestamps_to_students.rb
@@ -0,0 +1,7 @@
+class AddTimestampsToStudents < ActiveRecord::Migration
+  def change
+    change_table :students do |t|
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151219032754) do
+ActiveRecord::Schema.define(version: 20151219033106) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,8 +33,10 @@ ActiveRecord::Schema.define(version: 20151219032754) do
   end
 
   create_table "students", force: :cascade do |t|
-    t.string "name"
-    t.date   "dob"
+    t.string   "name"
+    t.date     "dob"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
